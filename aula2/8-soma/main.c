afef9b57d289de50a610ca9bd0a61064586155d6
Lucas Fiduniv <61048699+lucasfiduniv@users.noreply.github.com>
2025-04-06 12:29:11 -0300

    feat: add subtraction, multiplication, and division operations to the addition program

--- aula2/8-soma/main.c
+++ aula2/8-soma/main.c
@@ -4,13 +4,21 @@ int main(void){
 int numero1;
 int numero2;
 int soma;
+int subtracao;
+int multiplicacao;
+int divisao;
 
-printf("Digite um numero inteiro: ");
+printf("Digite o primeiro numero inteiro: ");
 scanf("%d", &numero1);
-
-printf("Digite outro numero inteiro: ");
+printf("Digite o segundo numero inteiro: ");
 scanf("%d", &numero2);
 soma = numero1 + numero2;
+subtracao = numero1 - numero2;
+multiplicacao = numero1 * numero2;
+divisao = numero1 / numero2;
 printf("A soma de %d e %d eh: %d\n", numero1, numero2, soma);
+printf("A subtracao de %d e %d eh: %d\n", numero1, numero2, subtracao);
+printf("A multiplicacao de %d e %d eh: %d\n", numero1, numero2, multiplicacao);
+printf("A divisao de %d e %d eh: %d\n", numero1, numero2, divisao);
 
 }
